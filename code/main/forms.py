c9fc6d0f421acf3e1b19345f92a75ff9a8530ae8
ushiken <u5h1k3n@gmail.com>
2020-04-28 21:58:12 +0900

    integrated the content create and update function

--- code/main/forms.py
+++ code/main/forms.py
@@ -3,14 +3,24 @@ from django import forms
 from main.models import Content
 
 
-class ContentCreateForm(forms.ModelForm):
+class ContentForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super(ContentForm, self).__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs["placeholder"] = field.label
+        self.fields["thumbnail"].widget = forms.widgets.FileInput()
+        self.fields["thumbnail"].widget.attrs["style"] = "display: none;"
+
+
+class ContentCreateForm(ContentForm):
 
     class Meta:
         model = Content
         fields = ("content_type", "title", "description", "thumbnail", "url")
 
 
-class ContentUpdateForm(forms.ModelForm):
+class ContentUpdateForm(ContentForm):
 
     class Meta:
         model = Content
