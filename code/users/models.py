8abbaf0fd7721dd728ccd8a73b80e814d7751712
ushiken <u5h1k3n@gmail.com>
2020-04-28 19:52:27 +0900

    added profile and icon field to User model

--- code/users/models.py
+++ code/users/models.py
@@ -1,6 +1,10 @@
+import glob
+import os
+import random
 import uuid
 
 from django.db import models
+from django.conf import settings
 from django.contrib.auth.models import (
     AbstractBaseUser, PermissionsMixin, UserManager
 )
@@ -9,13 +13,29 @@ from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
 
 
+# デフォルトのアイコンのPATHをランダムに取得する
+def random_icon_path():
+    icons = glob.glob(f'{settings.MEDIA_ROOT}/default_icons/*')
+    random_index = random.randrange(0, len(icons))
+    icon_path = icons[random_index]
+    icon_rel_path = "/".join(icon_path.split('/')[3:])
+    return icon_rel_path
+
+
 class User(AbstractBaseUser, PermissionsMixin):
+
+    # 画像ファイルの保存場所
+    def get_image_path(self, filename):
+        name = str(uuid.uuid4())
+        extension = os.path.splitext(filename)[-1]
+        return 'icons/' + name + extension
+
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
     username_validator = UnicodeUsernameValidator()
 
     username = models.CharField(
-        _('username'),
-        max_length=150,
+        'ユーザーネーム',
+        max_length=50,
         unique=True,
         help_text=_('Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.'),  # noqa: E501
         validators=[username_validator],
@@ -23,6 +43,12 @@ class User(AbstractBaseUser, PermissionsMixin):
             'unique': _("A user with that username already exists."),
         },
     )
+    profile = models.TextField(
+        '自己紹介文', max_length=140, blank=True, null=True)
+    icon = models.ImageField(
+        'アイコン', blank=True, null=False,
+        upload_to=get_image_path, default=random_icon_path)
+
     first_name = models.CharField(_('first name'), max_length=150, blank=True)
     last_name = models.CharField(_('last name'), max_length=150, blank=True)
     email = models.EmailField(_('email address'), blank=True)
