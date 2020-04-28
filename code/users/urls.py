51067b76aca193a15940687273af3b9681832b3d
ushiken <u5h1k3n@gmail.com>
2020-04-28 18:20:22 +0900

    added profile page

--- code/users/urls.py
+++ code/users/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('login/', views.Login.as_view(), name='login'),
     path('logout/', views.Logout.as_view(), name='logout'),
     path('signup/', views.Signup.as_view(), name='signup'),
+    path('profile/<uuid:pk>/', views.ProfileView.as_view(), name='profile'),
 ]
--- code/users/views.py
+++ code/users/views.py
@@ -1,10 +1,13 @@
 from django.contrib.auth import login
 from django.contrib.auth.views import LoginView, LogoutView
 from django.http import HttpResponseRedirect
+from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic.edit import CreateView
+from django.views.generic import CreateView, ListView
 
-from .forms import LoginForm, SignUpForm
+from main.models import Content
+from users.models import User
+from users.forms import LoginForm, SignUpForm
 
 
 class Login(LoginView):
@@ -39,3 +42,31 @@ class Signup(CreateView):
         login(self.request, user)
         self.object = user
         return HttpResponseRedirect(self.get_success_url())
+
+
+class ProfileView(ListView):
+    template_name = "users/profile.html"
+    model = Content
+    paginate_by = 3
+
+    def get_queryset(self, pk):
+        queryset = self.model.objects.filter(creator__pk=pk)
+        queryset = queryset.order_by('-created_at')
+        return queryset
+
+    def get_context_data(self, pk, *args, object_list=None, **kwargs):
+        context = super().get_context_data(
+            *args, object_list=object_list, **kwargs)
+        try:
+            context["creator"] = User.objects.get(pk=pk)
+        except User.DoesNotExist:
+            context["creator"] = None
+        return context
+
+    def get(self, request, pk, *args, **kwargs):
+        self.object_list = self.get_queryset(pk)
+        context = self.get_context_data(pk)
+        if context["creator"] is None:
+            return render(request, '404.html', context, status=404)
+        else:
+            return self.render_to_response(context)
