c20958b6802e63f5b7822a9c3d99d39fd671bf7b
ushiken <u5h1k3n@gmail.com>
2020-05-02 16:10:11 +0900

    added contact form

--- code/users/forms.py
+++ code/users/forms.py
@@ -1,7 +1,11 @@
 from django import forms
+from django.conf import settings
 from django.contrib.auth.forms import (
     AuthenticationForm, UserCreationForm, PasswordChangeForm,
 )
+from django.core.mail import BadHeaderError, send_mail
+from django.http import HttpResponse
+from django.template.loader import render_to_string
 
 from users.models import User
 
@@ -47,3 +51,38 @@ class UserPasswordChangeForm(PasswordChangeForm):
         super(UserPasswordChangeForm, self).__init__(*args, **kwargs)
         for field in self.fields.values():
             field.widget.attrs['placeholder'] = field.label
+
+
+class ContactForm(forms.Form):
+    name = forms.CharField(
+        label='',
+        max_length=100,
+        widget=forms.TextInput(attrs={
+            'placeholder': "お名前",
+        }),
+    )
+    email = forms.EmailField(
+        label='',
+        widget=forms.EmailInput(attrs={
+            'placeholder': "メールアドレス",
+        }),
+    )
+    message = forms.CharField(
+        label='',
+        widget=forms.Textarea(attrs={
+            'placeholder': "お問い合わせ内容",
+        }),
+    )
+
+    def send_email(self):
+        subject = "お問い合わせ"
+        name = self.cleaned_data['name']
+        email = self.cleaned_data['email']
+        message = self.cleaned_data['message']
+        context = {"name": name, "email": email, "message": message}
+        mail_text = render_to_string("users/contact_email.txt", context)
+        recipient_list = [settings.EMAIL_HOST_USER]
+        try:
+            send_mail(subject, mail_text, email, recipient_list)
+        except BadHeaderError:
+            return HttpResponse("無効なヘッダが検出されました。")
--- code/users/urls.py
+++ code/users/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('login/', views.Login.as_view(), name='login'),
     path('logout/', views.Logout.as_view(), name='logout'),
     path('signup/', views.Signup.as_view(), name='signup'),
+    path('contact/', views.ContactFormView.as_view(), name='contact'),
     path('password/change/',
          views.UserPasswordChangeView.as_view(), name='password_change'),
     path('profile/<uuid:pk>/', views.ProfileView.as_view(), name='profile'),
