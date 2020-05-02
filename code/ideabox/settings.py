22a707237f848cdb19fa140bc98377fc0e840138
ushiken <u5h1k3n@gmail.com>
2020-05-02 14:35:43 +0900

    added settings to send email

--- code/ideabox/settings.py
+++ code/ideabox/settings.py
@@ -147,3 +147,11 @@ LOGOUT_REDIRECT_URL = '/'
 LOGIN_URL = 'users:login'
 
 AUTH_USER_MODEL = 'users.User'
+
+# メールサーバーへの接続設定
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", "")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", "")
+EMAIL_USE_TLS = True
